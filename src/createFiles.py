e1495ac9706f9d50e4b149d9eeac152eeac33072
DYPIXY <derick.kunz@gmail.com>
2021-05-23 13:46:32 -0300

    up

--- src/createFiles.py
+++ src/createFiles.py
@@ -9,11 +9,12 @@ class fil:
 
         typeOfFile = ""
         message = ""
-        howMuch = 1 + int(input("how much files to write?: "))
-        nameOf = input("choose a name for the files: ")
-
-        #if empty, create empty files, else, what mensage do you want? If empty no file extension, if message, is a txt
+        nameOf = ""
         
+        howMuch = 1 + int(input("how much files to write?: "))
+
+
+        #if empty, create empty files, else, what mensage do you want? If empty no file extension, if message, is a txt        
         def FilePro():
             empty = input("do you want empty files? [y/n]: ")
 
@@ -27,6 +28,7 @@ class fil:
                 print("\n you put a wrong input, please write y or yes, n or no \n")
                 FilePro()
         FilePro()
+
 
         #create a directory if it does not exists, remove everything inside if it already exists
         try:
@@ -46,15 +48,25 @@ class fil:
             print("")
 
         #create the files
-        import random
+        def name():
+            a = input("type 'r' for random files or write any name you want")            
+            if a.lower() == "y":
+                randname = True
+
+
         list = []
-        for vari in range(1, howMuch, 0) :
+        for vari in range(1, howMuch, 1):
             ran = random.randint(1, howMuch)
-            if ran not in list:
-                list.append(ran)
-                open(nameOf+str(ran)+typeOfFile,"w+").write(str(message))
-                vari -= 1      
-            else:
-                vari +=1
+            for i in list:    
+                if i == ran:
+                    vari -= 1
+                    continue                    
+        
+            list.append(ran)
+            open(nameOf+str(ran)+typeOfFile,"w+").write(str(message))        
+
+        for pri in list:
+
+
         print("end")
     start()
